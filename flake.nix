be5ed39302f7e6cad8ab30fa29d30c67e387a77b
Jakub Kopański <jakub@famisoft.pl>
2024-04-30 15:50:30 +0200

    failing bulid

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+  };
+
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ cargo2nix.overlays.default ];
+        };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.75.0";
+          packageFun = import ./Cargo.nix;
+          extraRustComponents = [ "clippy" "rustfmt" ];
+        };
+
+        workspaceShell = rustPkgs.workspaceShell {
+          packages = with pkgs; [
+            cargo2nix.packages."${system}".cargo2nix
+            rust-analyzer
+          ];
+        };
+
+      in rec {
+        packages = {
+          issue = (rustPkgs.workspace.issue {}).out;
+          default = packages.issue;
+        };
+
+        devShells = {
+          default = workspaceShell;
+        };
+      }
+    );
+}
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub struct Dummy {
+    name: String,
+    nick: String,
+}
